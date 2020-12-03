6febe626dc72bb54241379cab32add96ab6c5139
mitty009 <mitchell.thomas808@gmail.com>
2020-12-03 16:55:38 +0300

    minor changes

--- backend/controllers/products.py
+++ backend/controllers/products.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from models.product import Product
+from models.product import ProductModel
 # from serializers.defect import DefectsSerializer
 from marshmallow import ValidationError
 
@@ -7,5 +7,5 @@ router = Blueprint(__name__, 'products')
 
 @router.route('/products', methods=['GET'])
 def get_products():
-  return 'Yo bro', 201
+  return "Yo bro"
   
