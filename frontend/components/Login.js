fc01d96ed45552119ed71ea415a513fb3762d037
danielzt94 <danieltockan@gmail.com>
2020-12-08 15:07:10 +0000

    log in and register funcitonality

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -1,8 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { getUserId, isCreator } from '../lib/auth'
 
-const Login = () => {
+const Login = (props) => {
 
-  return <h1>Hello Login</h1>
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const [errors, setErrors] = useState({
+    message: ''
+  })
+
+  function handleChange(event) {
+    const data = {
+      ...formData,
+      [event.target.name]: event.target.value
+    }
+    setFormData(data)
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    axios.post('/api/login', formData)
+      .then(resp => {
+        if (!resp.data.message) {
+          setErrors(resp.data)
+        } else {
+          localStorage.setItem('token', resp.data.token)
+          props.history.push('/myaccount')       
+        }
+      })
+  }
+  console.log(props)
+  
+
+  return <div>
+    <form
+      onSubmit={handleSubmit}
+    >
+      <div className="form-group">
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Email"
+          onChange={handleChange}
+          value={formData.email}
+          name="email"
+          required />
+      </div>
+
+      <div className="form-group">
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Password"
+          onChange={handleChange}
+          value={formData.password}
+          name="password"
+          required />
+      </div>
+
+      {errors.message && <p id="error" style={{ color: 'black' }}>
+        {errors.message}
+      </p>}
+
+      <button
+        className="btn btn-dark">
+          Login
+      </button>
+
+    </form>
+  </div>
 }
 
 export default Login
