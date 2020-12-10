d311b99aacedee3ad2b839fed0fe7fcda8c46c96
danielzt94 <danieltockan@gmail.com>
2020-12-10 16:56:24 +0000

    before heroku

--- backend/seeds.py
+++ backend/seeds.py
@@ -46,11 +46,11 @@ with app.app_context():
     # ("Business, Industry & Science", "https://www.amazon.co.uk/Best-Sellers-Business-Industry-Science/zgbs/industrial/ref=zg_bs_nav_0/261-3310178-9376341"),
     # ("CD's and Vinyl", "https://www.amazon.co.uk/Best-Sellers-Music/zgbs/music/ref=zg_bs_nav_0/261-3310178-9376341"),
     # ("Clothing", "https://www.amazon.co.uk/Best-Sellers-Clothing/zgbs/clothing/ref=zg_bs_nav_0/261-3310178-9376341"),
-    ("Computers and Accessories", "https://www.amazon.co.uk/Best-Sellers-Computers-Accessories/zgbs/computers/ref=zg_bs_nav_0/261-3310178-9376341"),
+    ("Computers and Accessories", "https://www.amazon.co.uk/Best-Sellers-Computers-Accessories/zgbs/computers/ref=zg_bs_nav_0/261-3310178-9376341")
     # ("DIY & Tools", "https://www.amazon.co.uk/Best-Sellers-DIY-Tools/zgbs/diy/ref=zg_bs_nav_0/261-3310178-9376341"),
     # ("DVD & Blu-ray", "https://www.amazon.co.uk/Best-Sellers-DVD-Blu-ray/zgbs/dvd/ref=zg_bs_nav_0/261-3310178-9376341"),
     # ("Digital Music", "https://www.amazon.co.uk/Best-Sellers-MP3-Downloads/zgbs/dmusic/ref=zg_bs_nav_0/261-3310178-9376341"),
-    ("Electronics & Photo", "https://www.amazon.co.uk/Best-Sellers-Electronics/zgbs/electronics/ref=zg_bs_nav_0/261-3310178-9376341"),
+    # ("Electronics & Photo", "https://www.amazon.co.uk/Best-Sellers-Electronics/zgbs/electronics/ref=zg_bs_nav_0/261-3310178-9376341"),
     # ("Garden & Outdoor", "https://www.amazon.co.uk/Best-Sellers-Garden-Outdoors/zgbs/outdoors/ref=zg_bs_nav_0/261-3310178-9376341"),
     # ("GIft Cards", "https://www.amazon.co.uk/Best-Sellers-Gift-Cards-Top-Up/zgbs/gift-cards/ref=zg_bs_nav_0/261-3310178-9376341"),
     # ("Grocery", "https://www.amazon.co.uk/Best-Sellers-Grocery/zgbs/grocery/ref=zg_bs_nav_0/261-3310178-9376341"),
@@ -62,15 +62,15 @@ with app.app_context():
     # ("Large Appliances", "https://www.amazon.co.uk/Best-Sellers-Large-Appliances/zgbs/appliances/ref=zg_bs_nav_0/261-3310178-9376341"),
     # ("Lighting", "https://www.amazon.co.uk/Best-Sellers-Lighting/zgbs/lighting/ref=zg_bs_nav_0/261-3310178-9376341"),
     # ("Luggage", "https://www.amazon.co.uk/Best-Sellers-Luggage/zgbs/luggage/ref=zg_bs_nav_0/261-3310178-9376341"),
-    ("Musical Instruments & DJ", "https://www.amazon.co.uk/Best-Sellers-Musical-Instruments/zgbs/musical-instruments/ref=zg_bs_nav_0/261-3310178-9376341"),
-    ("PC & Video Games", "https://www.amazon.co.uk/Best-Sellers-PC-Video-Games/zgbs/videogames/ref=zg_bs_nav_0/261-3310178-9376341"),
+    # ("Musical Instruments & DJ", "https://www.amazon.co.uk/Best-Sellers-Musical-Instruments/zgbs/musical-instruments/ref=zg_bs_nav_0/261-3310178-9376341"),
+    # ("PC & Video Games", "https://www.amazon.co.uk/Best-Sellers-PC-Video-Games/zgbs/videogames/ref=zg_bs_nav_0/261-3310178-9376341"),
     # ("Pet Supplies", "https://www.amazon.co.uk/Best-Sellers-Pet-Supplies/zgbs/pet-supplies/ref=zg_bs_nav_0/261-3310178-9376341"),
     # ("Shoes & Bags", "https://www.amazon.co.uk/Best-Sellers-Shoes-Bags/zgbs/shoes/ref=zg_bs_nav_0/261-3310178-9376341"),
-    ("Software", "https://www.amazon.co.uk/Best-Sellers-Software/zgbs/software/ref=zg_bs_nav_0/261-3310178-9376341"),
-    ("Sports & Outdoor", "https://www.amazon.co.uk/Best-Sellers-Sports-Outdoors/zgbs/sports/ref=zg_bs_nav_0/261-3310178-9376341"),
+    # ("Software", "https://www.amazon.co.uk/Best-Sellers-Software/zgbs/software/ref=zg_bs_nav_0/261-3310178-9376341"),
+    # ("Sports & Outdoor", "https://www.amazon.co.uk/Best-Sellers-Sports-Outdoors/zgbs/sports/ref=zg_bs_nav_0/261-3310178-9376341"),
     # ("Stationary & Office Supplies", "https://www.amazon.co.uk/Best-Sellers-Office-Products/zgbs/officeproduct/ref=zg_bs_nav_0/261-3310178-9376341"),
     # ("Toys & Games", "https://www.amazon.co.uk/Best-Sellers-Toys-Games/zgbs/kids/ref=zg_bs_nav_0/261-3310178-9376341"),
-    ("Watches", "https://www.amazon.co.uk/Best-Sellers-Watches/zgbs/watch/ref=zg_bs_nav_0/261-3310178-9376341")
+    # ("Watches", "https://www.amazon.co.uk/Best-Sellers-Watches/zgbs/watch/ref=zg_bs_nav_0/261-3310178-9376341")
   ]
 
   [get_product(name, url) for name, url in bestsellers_list]
