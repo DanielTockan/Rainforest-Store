7962b2b392b6c17f757589e233c1dc723d927d42
mitty009 <mitchell.thomas808@gmail.com>
2020-12-10 19:00:40 +0300

    deployment ready

--- backend/environment/config.py
+++ backend/environment/config.py
@@ -1,4 +1,4 @@
 import os
 db_URI = os.getenv('DATABASE_URL', 'postgres://localhost:5432/ecommerce_db')
 secret = os.getenv('SECRET', 'My mum has a brilliant mind... When shes drunk')
-API_KEY = "50B20C4B3A16406498FE9ABB28667CD2"
+API_KEY = os.getenv('API_KEY', '50B20C4B3A16406498FE9ABB28667CD2')
