ecf336bae41587958aba0e09715c99df4a32e39a
danielzt94 <danieltockan@gmail.com>
2020-12-10 11:55:55 +0000

    compilation erros fixed after merge

--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import 'bulma'
+import './styles/style.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(<App/>, document.getElementById('root'))
