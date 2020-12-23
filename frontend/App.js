0b73ccadd258b989f38b6857a26fe3498c3fdceb
danielzt94 <danieltockan@gmail.com>
2020-12-23 23:50:08 +0000

    added space to App.js for test

--- frontend/App.js
+++ frontend/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Switch, Link, Route } from 'react-router-dom'
 import './styles/style.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-
 // import Home from './components/Home'
 import MyDetails from './components/MyDetails'
 import MyOrders from './components/MyOrders'
